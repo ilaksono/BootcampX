34e6a0a6493c4ff1f543bcc908c18a952b92a973
ilaksono <ian.laksono@gmail.com>
2020-10-12 18:22:04 +0000

    add having queries

--- 2_queries_joins/Cohort's-Total-Assignment-Duration.sql
+++ 2_queries_joins/Cohort's-Total-Assignment-Duration.sql
@@ -1,2 +1,12 @@
 SELECT SUM(assignment_submissions.duration) FROM assignment_submissions
 JOIN students ON (student_id=students.id) JOIN cohorts ON (cohort_id=cohorts.id) GROUP BY cohorts.name HAVING cohorts.name='FEB12'; 
+
+
+
+
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL GROUP BY students.name HAVING count(assignment_submissions.*) < 100;
+
