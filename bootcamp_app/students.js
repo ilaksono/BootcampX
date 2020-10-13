302a958f06e7608a0fd9f6cc00ca2e270df88674
ilaksono <ian.laksono@gmail.com>
2020-10-13 20:24:23 +0000

    add parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -21,17 +21,29 @@ const pool = new Pool({
 const args = process.argv.slice(2);
 const querName = args[0];
 const querLimit = args[1];
-
-pool.query(`
-SELECT students.id, students.name as stud_name, cohorts.name as cohorts_name
+const queryString = `SELECT students.id, students.name as stud_name, cohorts.name as cohorts_name 
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${querName}%'
-LIMIT ${querLimit};
-`)
-.then(res => {
+WHERE cohorts.name LIKE $1
+LIMIT $2`;
+
+pool.query(queryString, [`%${querName}%`, querLimit]).then(res => {
   for( let entry of res.rows) {
-    console.log(`${entry.stud_name} has an if of ${entry.id} and was in the ${entry.cohorts_name} cohort`);
+    console.log(`${entry.stud_name} has an id of ${entry.id} and was in the ${entry.cohorts_name} cohort`);
   }
 })
-.catch(err => console.error('query error', err.stack));
+.catch(err => console.error('query error', err.stack));;
+
+// pool.query(`
+// SELECT students.id, students.name as stud_name, cohorts.name as cohorts_name
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${querName}%'
+// LIMIT ${querLimit};
+// `)
+// .then(res => {
+//   for( let entry of res.rows) {
+//     console.log(`${entry.stud_name} has an id of ${entry.id} and was in the ${entry.cohorts_name} cohort`);
+//   }
+// })
+// .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,17 +10,39 @@ const pool = new Pool({
 const args = process.argv.slice(2);
 const querCoh = args[0];
 
-pool.query(`
-SELECT cohorts.name as ch_name, teachers.name as tch_name FROM teachers
+const queryString = `SELECT cohorts.name as ch_name, teachers.name as tch_name FROM teachers
 JOIN assistance_requests ON (teachers.id = teacher_id)
 JOIN students ON (student_id = students.id)
 JOIN cohorts ON (cohorts.id=cohort_id) 
-WHERE cohorts.name LIKE '%${querCoh}%'
-LIMIT 10;
-`)
-.then(res => {
+WHERE cohorts.name LIKE $1
+LIMIT 10;`
+
+pool.query(queryString, [`%${querCoh}%`]).then(res => {
   for( let entry of res.rows) {
     console.log(`${entry.ch_name}: ${entry.tch_name}`);
   }
 })
 .catch(err => console.error('query error', err.stack));
+
+// pool.query(`
+// SELECT cohorts.name as ch_name, teachers.name as tch_name FROM teachers
+// JOIN assistance_requests ON (teachers.id = teacher_id)
+// JOIN students ON (student_id = students.id)
+// JOIN cohorts ON (cohorts.id=cohort_id) 
+// WHERE cohorts.name LIKE '%${querCoh}%'
+// LIMIT 10;
+// `)
+// .then(res => {
+//   for( let entry of res.rows) {
+//     console.log(`${entry.ch_name}: ${entry.tch_name}`);
+//   }
+// })
+// .catch(err => console.error('query error', err.stack));
+
+
+// SELECT cohorts.name as ch_name, teachers.name as tch_name FROM teachers
+// JOIN assistance_requests ON (teachers.id = teacher_id)
+// JOIN students ON (student_id = students.id)
+// JOIN cohorts ON (cohorts.id=cohort_id) 
+// WHERE cohorts.name LIKE '%FEB '2; UPDATE students SET email = 'hacker@hacked.me' WHERE id = 1'%'
+// LIMIT 10;
