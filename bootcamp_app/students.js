2e10ecb8713b15325d284c312b3a5e2781ee5442
ilaksono <ian.laksono@gmail.com>
2020-10-13 19:38:27 +0000

    add psql node app

---
+++ bootcamp_app/students.js
@@ -0,0 +1,37 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+// pool.query(`
+// SELECT students.id, students.name as stud_name, cohorts.name as cohorts_name
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// LIMIT 5;
+// `)
+// .then(res => {
+//   console.log(res.rows);
+// })
+// .catch(err => console.error('query error', err.stack));
+
+const args = process.argv.slice(2);
+const querName = args[0];
+const querLimit = args[1];
+
+pool.query(`
+SELECT students.id, students.name as stud_name, cohorts.name as cohorts_name
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${querName}%'
+LIMIT ${querLimit};
+`)
+.then(res => {
+  for( let entry of res.rows) {
+    console.log(`${entry.stud_name} has an if of ${entry.id} and was in the ${entry.cohorts_name} cohort`);
+  }
+})
+.catch(err => console.error('query error', err.stack));
